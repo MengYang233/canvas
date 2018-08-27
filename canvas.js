ee8c35eff5e0fc03c54035a613aa38e9c2b2a32a
MengYang233 <42724382+MengYang233@users.noreply.github.com>
2018-08-28 07:47:37 +0800

    Update canvas.js

--- canvas.js
+++ canvas.js
@@ -1,15 +1,37 @@
 canvas = document.getElementById("canvas");
 context = canvas.getContext("2d");
 context.lineWidth = 1;
-canvas.onmousedown = function() {
-    old_X = event.clientX;
-    old_Y = event.clientY;
-    context.moveTo(old_X, old_Y);
+var count = 0;
+window.onload = function()
+{
+	canvas.setAttribute("width",window.innerWidth)
+	canvas.setAttribute("height",window.innerHeight)
 }
-canvas.onmouseup = function() {
-    new_X = event.clientX;
-    new_Y = event.clientY;
-    context.lineTo(new_X,new_Y);
-    context.stroke();
-    context.closePath();
+function hua() 
+{
+new_X = event.clientX;
+new_Y = event.clientY;
+if(count >= 0.2)
+{
+	context.moveTo(new_X, new_Y);
+	count = 0;
 }
+context.lineTo(new_X, new_Y);
+context.stroke();
+context.closePath();
+count+=0.05;
+}
+canvas.onmousemove = function() {
+	hua();
+}
+function clearCanvas()
+{
+	canvas.height=canvas.height;
+}
+
+
+
+
+
+
+
